1b46bbde33d4e1e8db5cb92bcce2c775fa980104
Richard Lee <richlee81192@gmail.com>
2019-08-20 17:42:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,11 +31,11 @@ end
 def hit?(total)
   prompt_user
   get_user_input
-  if get_user_input == 'h'
-    total += deal_card
-    
-  elsif get_user_input == 's'
-  
+  if get_user_input == 's'
+    return total
+  elsif get_user_input == 'h'
+  total += deal_card
+  get_user_input
   else
   invalid_command
   prompt_user
