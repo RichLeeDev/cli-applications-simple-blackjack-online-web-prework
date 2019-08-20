2c3f427372ebe34f9e234d3c2ad707c291e996cf
Richard Lee <richlee81192@gmail.com>
2019-08-20 17:46:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,14 +29,14 @@ def initial_round
 end
 
 def hit?(total)
+  
   prompt_user
   get_user_input
   
   if get_user_input = 's'
     return total
   elsif get_user_input = 'h'
-  deal_card
-  total += deal_card
+  
   return total
   else
   invalid_command
